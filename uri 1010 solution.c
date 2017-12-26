10d451eb6f6694db8a49e68ffa52687b5430ccc2
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2017-12-27 03:18:12 +0600

    Create uri 1010 solution.c

---
+++ uri 1010 solution.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int c1,c2,u1,u2;
+	float p1,p2;
+	scanf("%d %d %f",&c1,&u1,&p1);
+	scanf("%d %d %f",&c2,&u2,&p2);
+	printf("VALOR A PAGAR: R$ %.2f\n",(u1*p1)+(u2*p2));
+	return 0;
+}
